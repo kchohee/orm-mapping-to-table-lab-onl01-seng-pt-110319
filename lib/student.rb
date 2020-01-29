9c6b98ac77bc53155aca48a84b67d10cd6d097ff
Marie (Cho) Hughes <kchohee0630@gmail.com>
2020-01-29 22:09:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,9 +17,9 @@ class Student
   end
   def self.drop_table
     sql = <<-SQL
-    DORP TABLE students
-    SQL 
-    DB[:conn].excute(SQL)
+    DROP TABLE students
+    SQL
+    DB[:conn].execute(sql)
   end
   def save
   end
